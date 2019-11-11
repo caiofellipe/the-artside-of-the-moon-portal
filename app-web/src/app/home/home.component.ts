06e2c26faf161012c91841b62af38b1ded3de508
Caio Oliveira <caiofellipe80@gmail.com>
2019-11-11 03:30:01 -0300

    Configuração do componente

--- app-web/src/app/home/home.component.ts
+++ app-web/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var require: any
+require('paroller.js');
 
 @Component({
   selector: 'moon-home',
@@ -10,6 +13,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.my-paroller-rocket').paroller();
+
+    $(".my-rocket").paroller({ factor: 0.5, factorXs: 0.2, factorSm: 0.3, type: 'foreground', direction: 'horizontal' });
   }
 
 }
